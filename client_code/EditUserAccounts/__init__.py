1316ef568454d2bb5c3fd02247b60d5cd17d3fc3
robkam007@gmail.com <robkam007@gmail.com>
2025-09-03 05:47:56 +0000

    Created EditUserAccounts; Edited UserAccounts.RowTemplate14 and UserAccounts

--- client_code/EditUserAccounts/__init__.py
+++ client_code/EditUserAccounts/__init__.py
@@ -1,20 +1,24 @@
-from ._anvil_designer import RowTemplate14Template
+from ._anvil_designer import EditUserAccountsTemplate
 from anvil import *
 import anvil.server
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ...EditUserAccounts import EditUserAccounts
 
 
-class RowTemplate14(RowTemplate14Template):
+class EditUserAccounts(EditUserAccountsTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
 
-    def btn_Edit_click(self, **event_args):
+    def btn_Update_click(self, **event_args):
         """This method is called when the button is clicked"""
-        alert(content=EditUserAccounts(), buttons=[], dismissible=False, large=True)
+        pass
+
+    def btn_Close_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.raise_event("x-close-alert", value=True)
+

7c64720b3861bc2a7f57cf4d74e618de058e41a0
robkam007@gmail.com <robkam007@gmail.com>
2025-09-12 06:49:55 +0000

    Edited MapBarCodePartNo

--- client_code/MapBarCodePartNo/__init__.py
+++ client_code/MapBarCodePartNo/__init__.py
@@ -24,3 +24,35 @@ class MapBarCodePartNo(MapBarCodePartNoTemplate):
         """This method is called when the button is clicked"""
         alert(content=AddNewParts(isPopup=True),buttons=[], dismissible=True, large=True)
 
+    def btn_Search_click(self, **event_args):
+        """This method is called when the text in this text box is edited"""
+        search_value = self.text_box_searchPartNo.text.strip()
+
+        if not search_value:
+            alert("Please enter part name or part no. to proceed.", title="Blank Field(s) Found", large=False)
+            self.text_box_searchPartNo.focus()
+            return
+
+        result = anvil.server.call('getCarPartNameAndNumber', search_value)
+
+        #Clear drop down 
+        self.drop_down_selectPart.items = ""
+
+        if result:
+            self.drop_down_selectPart.items = result
+        else:
+            alert("No records found for the entered part detail.", title="Not Found")
+
+    def drop_down_selectPart_change(self, **event_args):
+        """This method is called when an item is selected"""
+        result = anvil.server.call_s("getCarPartNumberWithID", self.drop_down_selectPart.selected_value)
+        self.lbl_PartNumber.text = result[0]["PartNo"]
+
+    def btn_Save_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        barcode = self.txt_BarCode.text.strip()
+        partNo = self.lbl_PartNumber.text.strip()
+
+        
+
+

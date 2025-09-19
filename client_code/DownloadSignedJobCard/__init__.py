3030b481d026320d18e0ee06e17dae1b177b2f33
robkam007@gmail.com <robkam007@gmail.com>
2025-09-19 05:35:11 +0000

    Edited DownloadSignedJobCard

--- client_code/DownloadSignedJobCard/__init__.py
+++ client_code/DownloadSignedJobCard/__init__.py
@@ -6,6 +6,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.js
+import base
 
 class DownloadSignedJobCard(DownloadSignedJobCardTemplate):
     def __init__(self, **properties):

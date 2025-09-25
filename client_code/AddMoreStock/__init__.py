16fadff0a2d4d55aa017040193d3a5c2088ac7a5
robkam007@gmail.com <robkam007@gmail.com>
2025-09-25 13:52:16 +0000

    Edited AddMoreStock

--- client_code/AddMoreStock/__init__.py
+++ client_code/AddMoreStock/__init__.py
@@ -25,6 +25,8 @@ class AddMoreStock(AddMoreStockTemplate):
             self.handle_server_errors
         )  # Set global server error handler
 
+        self.drop_down_selectSupplier.items = anvil.server.call("getSupplier")
+        
     def handle_server_errors(self, exc):
         if isinstance(exc, anvil.server.UplinkDisconnectedError):
             anvil.alert(
@@ -71,6 +73,7 @@ class AddMoreStock(AddMoreStockTemplate):
         self.btn_SaveAndNew.enabled = False  # Disable button to prevent multiple clicks
 
         additionDate = self.date_picker_1.date
+        supplier = self.drop_down_selectSupplier.selected_value
         partID = self.drop_down_selectPart.selected_value
         no_of_units = self.txt_NoOfUnits.text.strip().upper()
         unit_cost = self.txt_UnitCost.text.strip()
@@ -78,6 +81,12 @@ class AddMoreStock(AddMoreStockTemplate):
         if not additionDate:
             alert("Please enter date to proceed.")
             self.additionDate.focus()
+            self.btn_SaveAndNew.enabled = True
+            return
+
+        if not supplier:
+            alert("Please select supplier to proceed.")
+            self.drop_down_selectSupplier.focus()
             self.btn_SaveAndNew.enabled = True
             return
             
@@ -94,7 +103,7 @@ class AddMoreStock(AddMoreStockTemplate):
             return
 
         # Save data
-        anvil.server.call("addStock", additionDate, partID,  no_of_units, unit_cost)
+        anvil.server.call("addStock", additionDate, supplier, partID,  no_of_units, unit_cost)
         alert("Stock added successfully.")
 
         # Clear form
@@ -115,11 +124,13 @@ class AddMoreStock(AddMoreStockTemplate):
     def clear_form_fields(self):
         """Reset all fields after saving"""
         self.date_picker_1.date = None
+        self.drop_down_selectSupplier.items =[]
         self.text_box_searchPartNo.text = ""
         self.drop_down_selectPart.items = []
         self.drop_down_selectPart.selected_value = None
         self.txt_NoOfUnits.text = ""
         self.txt_UnitCost.text = ""
+        self.repeating_panel_1.items = []
 
         # Put cursor back on search box for next entry
         self.text_box_searchPartNo.focus()
@@ -127,4 +138,4 @@ class AddMoreStock(AddMoreStockTemplate):
         self.refresh()
         self.btn_SaveAndNew.enabled =True
 
-    
+   

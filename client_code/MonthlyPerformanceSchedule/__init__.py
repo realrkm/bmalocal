53d1201ed793f2d9fea5516589dfca0a1fab2675
robkam007@gmail.com <robkam007@gmail.com>
2025-10-01 07:14:32 +0000

    Edited MonthlyPerformanceSchedule

--- client_code/MonthlyPerformanceSchedule/__init__.py
+++ client_code/MonthlyPerformanceSchedule/__init__.py
@@ -13,3 +13,22 @@ class MonthlyPerformanceSchedule(MonthlyPerformanceScheduleTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+    
+    def btn_Search_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        startDate = self.date_picker_start.date
+        endDate = self.date_picker_end.date
+        searchTerm = self.txt_JobCardRef.text
+
+        if not startDate or not endDate:
+            alert("Sorry, please enter date period to proceed", title="Blank Field(s) Found")
+            return
+        if startDate > endDate:
+            alert("Sorry, start date cannot be greater than end date", title="Mismatch Dates")
+            return
+        if not searchTerm:
+            alert("Sorry, please enter job card ref to proceed", title="Blank Field(s) Match")
+            self.txt_JobCardRef.focus()
+            return
+            
+        self.repeating_panel_1.items = anvil.server.call("getPeriodicInvoices", startDate, endDate, searchTerm)

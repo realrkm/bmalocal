cb6f508fb44a5a8fc501807c48deb92c7a0b1cea
robkam007@gmail.com <robkam007@gmail.com>
2025-10-08 09:02:14 +0000

    Edited TransitionInterimQuoteToInvoice

--- client_code/TransitionInterimQuoteToInvoice/__init__.py
+++ client_code/TransitionInterimQuoteToInvoice/__init__.py
@@ -13,3 +13,28 @@ class TransitionInterimQuoteToInvoice(TransitionInterimQuoteToInvoiceTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+
+    def btn_Search_click(self, **event_args):
+        """This method is called when the text in this text box is edited"""
+        if self.txtJobCardRef.text:
+            search_value = self.txtJobCardRef.text.strip()
+            result = anvil.server.call("getInterimQuote", search_value)
+            # Clear drop down
+            self.drop_down_JobCardRefDetails.items = ""
+
+            self.drop_down_JobCardRefDetails.items = result
+        else:
+            alert("Please enter job card ref to proceed.", title="Blank Field(s) Found", large=False)
+            self.txtJobCardRef.focus()
+
+    def btn_Transition_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        if self.drop_down_JobCardRefDetails.selected_value:
+            anvil.server.call("transitionInterimQuoteToInvoice", self.drop_down_JobCardRefDetails.selected_value)
+            alert("Interim Quote Transitioned To Invoice", title="Success")
+            #Close Form
+            self.raise_event("x-close-alert", value=True)
+        else:
+            alert("Sorry, please select the jobcard ref to proceed", title="Blank Foeld(s) Found")
+            
+            

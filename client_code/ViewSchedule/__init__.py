7e231f9d4e9be6afb894e5bb9d9c0bae26c15091
robkam007@gmail.com <robkam007@gmail.com>
2025-10-08 04:55:17 +0000

    Created ViewSchedule.RowTemplate20 and ViewSchedule; Deleted Form1.RowTemplate20 and Form1

--- client_code/ViewSchedule/__init__.py
+++ client_code/ViewSchedule/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import ViewScheduleTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -7,9 +7,13 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class ViewSchedule(ViewScheduleTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+
+    def btn_Search_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        start

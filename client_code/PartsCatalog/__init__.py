6f5c966771d0f5965f1546e452d833cbc71387d5
robkam007@gmail.com <robkam007@gmail.com>
2025-10-28 08:28:55 +0000

    Edited PartsCatalog

--- client_code/PartsCatalog/__init__.py
+++ client_code/PartsCatalog/__init__.py
@@ -16,7 +16,7 @@ class PartsCatalog(PartsCatalogTemplate):
 
         # Any code you write here will run before the form opens.
         # Create an iframe element and set the src
-        iframe = jQuery("<iframe width='100%' height='800px'>").attr("src","https://nemigaparts.com/cat_spares/")
+        iframe = jQuery("<iframe width='100%' height='800px'>").attr("src","https://www.realoem.com/")
     
         # Append the iframe to a container in our form
         iframe.appendTo(get_dom_node(self.content_panel))

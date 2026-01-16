9493a5cc53485dcb2034d543050fcb4c4836ec9c
robkam007@gmail.com <robkam007@gmail.com>
2026-01-16 04:15:47 +0000

    Edited Main

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -182,11 +182,7 @@ class Main(MainTemplate):
         self.refresh()
         notifications = anvil.server.call('fetch_role_notifications', anvil.users.get_user())
         for n in notifications:
-            Notification(
-                f"{n['jobcard']} {n['message']}",
-                style="success",
-                timeout=None
-            ).show()
+            #Notification(f"{n['jobcard']} {n['message']}",style="success",timeout=None).show()
             self.label_1.text=f"{n['jobcard']} {n['message']}"
             #self.notification_timer.interval=anvil.server.call_s("delete_notification")
         

675cab29b53c2a6cd6598875037746558794c5d3
robkam007@gmail.com <robkam007@gmail.com>
2026-01-29 13:33:05 +0000

    Edited SelfService

--- client_code/SelfService/__init__.py
+++ client_code/SelfService/__init__.py
@@ -14,7 +14,7 @@ class SelfService(SelfServiceTemplate):
 
         # Any code you write here will run before the form opens.
         anvil.js.call('replaceBanner')
-        while anvil.users.get_user() is None:
+        #while anvil.users.get_user() is None:
             anvil.users.login_with_form()
         #result = anvil.server.call("update_carpart_categories")
         #alert(f"Updated {result['updated_rows']} rows")

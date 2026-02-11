68e85579a0c293ae66949313cc1aef1aed44df83
robkam007@gmail.com <robkam007@gmail.com>
2026-02-11 11:43:02 +0000

    Edited ModNavigation

--- client_code/ModNavigation/__init__.py
+++ client_code/ModNavigation/__init__.py
@@ -99,11 +99,3 @@ def go_Enable_Incomplete_Defects_Info():
     form = get_open_form()
     form.btn_IncompleteDefectsInfo.enabled = True
 
-#Load View Update Parts Button
-def go_Disable_Requested_Parts_Info():
-    form = get_open_form()
-    form.btn_UpdatedRequestedParts.enabled = False
-
-def go_Enable_Requested_Parts_Info():
-    form = get_open_form()
-    form.btn_UpdatedRequestedParts.enabled = True

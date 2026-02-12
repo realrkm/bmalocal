62ea9ad45d19d13b23a2c849225c9ee9a757d280
robkam007@gmail.com <robkam007@gmail.com>
2026-02-12 12:56:19 +0000

    Edited DefectsForm

--- client_code/DefectsForm/__init__.py
+++ client_code/DefectsForm/__init__.py
@@ -165,6 +165,7 @@ class DefectsForm(DefectsFormTemplate):
         self.downloadDefectsPdf(jobcardID)
               
     def downloadDefectsPdf(self, job_card_id):
+        print(job_card_id)
         media_object = anvil.server.call('downloadDefectsPdfForm', job_card_id, "DefectsList")
         anvil.media.download(media_object)
         self.deleteFile(job_card_id, "DefectsList")

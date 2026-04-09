8ce1fb913f9914851963ebc330915b65d0689298
robkam007@gmail.com <robkam007@gmail.com>
2026-04-09 15:56:20 +0000

    Edited ViewPricingAlertDetails.RowTemplate and ViewPricingAlertDetails

--- client_code/ViewPricingAlertDetails/RowTemplate/__init__.py
+++ client_code/ViewPricingAlertDetails/RowTemplate/__init__.py
@@ -5,28 +5,16 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ...UpdatePricingAmount import UpdatePricingAmount
 
 class RowTemplate(RowTemplateTemplate):
     def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
         self.init_components(**properties)
         self.user = anvil.users.get_user()
 
-        # Any code you write here will run before the form opens.
-
     def btn_EditItem_click(self, **event_args):
         """This method is called when the button is clicked"""
-        items = list(self.parent.items)
-        partNo = items[list(self.parent.items).index(self.item)]['PartNo']
-        # Pass partNo up to ViewPricingAlertDetails instead of opening a nested alert
-        self.parent.raise_event("x-close-parent")
-        
-        alert(
-            content=UpdatePricingAmount(partNo),
-            buttons=[],
-            dismissible=False,
-            large=True
-        )
-        
 
+        partNo = self.item['PartNo']
+
+        # Call method directly on parent form
+        self.parent.parent.open_edit_form(partNo)
--- client_code/ViewPricingAlertDetails/__init__.py
+++ client_code/ViewPricingAlertDetails/__init__.py
@@ -7,21 +7,29 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.js
 from .. import ModNavigation
+from ..UpdatePricingAmount import UpdatePricingAmount
 
 class ViewPricingAlertDetails(ViewPricingAlertDetailsTemplate):
     def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
-        # Any code you write here will run before the form opens.
         anvil.js.call("replaceBanner")
         self.user = anvil.users.get_user()
+
         self.repeating_panel_1.items = anvil.server.call_s(
             "getPartsWhereBuyingPriceExceedsSelling", self.user
         )
-        self.set_event_handler("x-close-parent", self.Close_form)
-    
-    def Close_form(self, **event_args):
-        """This method is called when the button is clicked"""
+
+    def open_edit_form(self, partNo):
+        """Close this form and open UpdatePricingAmount"""
+
+        # Close this alert
         self.raise_event("x-close-alert", value=True)
-    
+
+        # Open next alert
+        alert(
+            content=UpdatePricingAmount(partNo),
+            buttons=[],
+            dismissible=False,
+            large=True
+        )

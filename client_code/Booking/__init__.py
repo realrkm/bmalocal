1bc35a37be62cffc6a1d901bddaf3f204976028a
robkam007@gmail.com <robkam007@gmail.com>
2026-04-25 10:36:01 +0000

    Edited Booking

--- client_code/Booking/__init__.py
+++ client_code/Booking/__init__.py
@@ -15,7 +15,8 @@ class Booking(BookingTemplate):
 
         # Any code you write here will run before the form opens.
         anvil.js.call('replaceBanner')#Set focus into technician
-
+        self.repeating_panel_1.items = anvil.server.call("getBookingDetails")
+        
     def btn_SearchCustomer_click(self, **event_args):
         """This method is called when the button is clicked"""
         valueCustomer = self.txt_ClientName.text
@@ -28,8 +29,42 @@ class Booking(BookingTemplate):
 
     def drop_down_selectCustomer_change(self, **event_args):
         """This method is called when an item is selected"""
-        if self.drop_down_selectCustomer.selected_value:
-            alert(self.drop_down_selectCustomer.selected_value)
-        #results = anvil.server.call("",self.drop_down_selectCustomer.selected_value)
+        # Fetch the list from the server
+        reg_numbers = anvil.server.call('getCarRegNoFromClientID', self.drop_down_selectCustomer.selected_value)
+    
+        # Set the items of your car registration dropdown
+        self.drop_down_car_reg.items = reg_numbers
 
-        
+    
+    def btn_Save_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        customer = self.drop_down_selectCustomer.selected_value
+        regno = self.drop_down_car_reg.selected_value
+        period = self.date_picker_period.date
+        details = self.txt_area_details.text
+    
+        # Check if ANY of the fields are missing (using 'or')
+        if not (customer and regno and period and details):
+            alert(
+                content="Please ensure all fields (Customer, Reg No, Date/Time, and Details) are filled in to proceed.",
+                title="Missing Information",
+                buttons=[("OK", None)],
+                dismissible=True
+            )
+        else:
+            # Call the server to save
+            anvil.server.call("saveBookingDetails", customer, regno, period, details)
+    
+            # Optional: Show success message and clear fields
+            alert("Booking saved successfully!")
+            self.repeating_panel_1.items = anvil.server.call("getBookingDetails")
+            self.clear_form()
+
+    def clear_form(self):
+        # It's good practice to reset the UI after a successful save
+        self.txt_ClientName.text=""
+        self.drop_down_selectCustomer.items = None
+        self.drop_down_car_reg.items = None
+        self.date_picker_period.date = None
+        self.txt_area_details.text = ""
+    
